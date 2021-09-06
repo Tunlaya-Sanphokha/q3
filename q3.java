a0d60d2e4aef4542dc16f8580f1bec90b1668404
Tunlaya-Sanphokha <s6201012620341@email.kmutnb.ac.th>
2021-09-06 22:50:06 +0700

    change checkwin method use for loop

---
+++ q3.java
@@ -0,0 +1,118 @@
+import java.util.*;       
+
+public class q3{
+    static String[][] boardGame = {{"1","2","3"},{"4","5","6"},{"7","8","9"}};
+    static int count = 0;
+    static String player = "x";
+    //static int pos;
+    static int row;
+    static int column;
+
+    public static void main(String[] args){  
+        play(); 
+        
+       
+    } /// void main
+
+
+    public static void play(){
+        
+        while(count < 9) {
+            System.out.print(System.lineSeparator());
+            System.out.println( "-- Tic Tac Toe Game --");
+            printBoard();
+
+            Scanner input = new Scanner(System.in);    
+            System.out.println("It's your turn , Please select from 0 - 2"  );  
+            System.out.print("Enter your row input: ");
+            
+            //int pos = input.nextInt(); 
+
+            try{
+                row = input.nextInt();
+                if(!(row >= 0 && row <=2)){
+                    continue;
+                }
+                System.out.print("Enter your column input: ");
+                column = input.nextInt();           
+                if(!(column >= 0 && column <=2)){
+                    continue;
+                }
+            }
+            catch(Exception e) {
+                System.out.println("Answer was wrong , Please Play Game Again Byeee"); // จะให้การทำงานหยุดแล้วผู้เล่น เล่นเกมใหม่ 
+                break;
+            }
+            
+           
+            while(boardGame[row][column] == "x" || boardGame[row][column] == "o"){  //ถ้าเคยใส่ตำแหน่งๆนี้ไปแล้ว 
+                System.out.println("Enter Again row:");    //ใส่ซ้ำอีกครั้ง
+                row = input.nextInt(); 
+                System.out.println("Enter Again column:");
+                column = input.nextInt();               
+            }
+            
+            boardGame[row][column] = player;        //เก็บ x หรือ o ใส่ในboardGame   
+                
+                if (check_win() ) {            //ถ้ามีคนชนะ 
+                    System.out.println("End Game");      //จบเกม
+                    break;                      //break
+                }
+                else {             
+                    count ++;   //ไม่มีผู้ชนะ count บวกเพิ่ม 
+                    if(player == "x"){   ///เปลี่ยน turn x เป็น turn o
+                        player = "o";
+                        }
+                    else {
+                        player = "x";
+                    }
+                }
+            } ///if boardGame[pos] == "x" 
+           
+        }// while 
+    //} 
+  //playGame
+    public static void printBoard(){ ///ฟังก์ชัน printBpardเอาไว้ วาด BorlGame
+        
+        System.out.println("|" + boardGame[0][0] + "|" + boardGame[0][1] + "|" +boardGame[0][2] + "|");  
+        System.out.println("-------");
+        System.out.println("|" + boardGame[1][0]+ "|" + boardGame[1][1] + "|" +boardGame[1][2] + "|");
+        System.out.println("-------");
+        System.out.println("|" + boardGame[2][0] + "|" + boardGame[2][1] + "|" +boardGame[2][2] + "|");
+        System.out.print(System.lineSeparator());   //ขึ้นบรรทัดใหม่ ให้บอร์ดดูง่ายขึ้นน 
+    }  ///printborad
+
+    public static boolean check_win() {          
+        //String[] checkPosition = new String[4];         
+        //boolean contain = false;   
+        for (int r = 0 ; r < 3 ; r ++){
+            if( boardGame[r][0] == boardGame[r][1] && boardGame[r][1] == boardGame[r][2] && boardGame[r][0] != " "){
+                printBoard();
+                System.out.println("congrats"+ " " + player + " " +"win");
+                return true;
+
+            }    
+        }
+        for (int c = 0 ; c < 3 ; c ++){
+        if(boardGame[0][c] == boardGame[1][c]&& boardGame[1][c] == boardGame[2][c] &&  boardGame[0][c] != " "){
+                printBoard();
+                System.out.println("congrats"+ " " + player + " " +"win");
+                return true;
+            }
+        }
+        if (boardGame[0][0] == boardGame[1][1] && boardGame[1][1] == boardGame[2][2] && boardGame[0][0] != " "){
+            printBoard();
+            System.out.println("congrats"+ " " + player + " " +"win");
+            return true;
+        }
+        if (boardGame[0][2] == boardGame[1][1] && boardGame[1][1] == boardGame [2][0] && boardGame[0][2] != " "){
+            printBoard();
+            System.out.println("congrats"+ " " + player + " " +"win");
+            return true;
+        }
+        return false;
+        
+
+    } //check_win
+
+}  ///class XO_Textmode
